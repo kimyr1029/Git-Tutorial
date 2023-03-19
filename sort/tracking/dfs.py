3bd9657eafdf5da9572a5cbdc1c9a8e0cf3382db
kimyr1029 <112699110+kimyr1029@users.noreply.github.com>
2023-03-20 07:12:00 +0900

    Update dfs.py

---
+++ sort/tracking/dfs.py
@@ -0,0 +1,23 @@
+#stack
+def dfs(graph,v,visited):
+    visited[v]=True
+    print(v,end=' ')
+    for i in graph[v]:
+        if not visited[v]:
+            dfs(graph,i,visited)
+n=int(input())
+visited=[False]*(n+1)
+graph=[
+
+    [],
+    [2,3,8],
+    [1,7],
+    [1,4,5],
+    [3,5],
+    [3,4],
+    [7],
+    [2,6,8],
+    [1,7]
+]
+
+dfs(graph,1,visited)
